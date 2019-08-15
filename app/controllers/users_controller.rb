73e79d56badb1a6b084f027e310ff14b0fef8724
github username <github email address>
2019-08-15 18:23:20 -0400

    backend auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,36 @@
 class UsersController < ApplicationController
+
+    def index
+        users = User.all 
+        render json: users     
+    end
+
+    def create
+        user = User.create(user_params)
+        if user.valid?
+            render json: auth_response_json(user) # see application_controller.rb
+        else
+            render json: { errors: user.errors.full_messages }
+        end 
+    end
+
+    def show
+        user_id = params[:id]
+        if authorized?(user_id) # see application_controller.rb
+            user = User.find(user_id)
+            render json: user
+        else
+            tell_user_to_go_away! # see application_controller.rb
+        end
+    end
+
+    def destroy
+        user = User.find(params[:id])
+        user.destroy()
+    end
+
+    private
+    def user_params
+        params.permit(:name, :user_name, :password)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
-    
+    has_secure_password
+    validates_presence_of :user_name
+    validates_uniqueness_of :user_name
 end
